6d0eda439822fa3d8b2305ab9682427e52cce34f
David <grantclan.7@gmail.com>
2025-08-04 15:04:19 +0100

    sidebar update

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -86,28 +86,28 @@ function Sidebar() {
             <ArrowRightStartOnRectangleIcon className="h-5 w-5"/>
             <p>Logout</p>
         </button>*/}
-        <button  className="flex items-center space-x-2 hover:text-white"  >
+        <button  className="flex items-center space-x-2 hover:text-green-500"  >
             <HomeIcon className="h-5 w-5"/>
             <p>Home</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white"  >
+        <button className="flex items-center space-x-2 hover:text-green-50"  >
             <MagnifyingGlassIcon className="h-5 w-5"/>
             <p>Search</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white" >
+        <button className="flex items-center space-x-2 hover:text-green-500" >
             < VscLibrary className="h-5 w-5"/>
             <p>Your Library</p>
         </button>
        <hr className="border-t-[0.1px] border-gray-900"></hr>
-       <button  className="flex items-center space-x-2 hover:text-white"  >
+       <button  className="flex items-center space-x-2 hover:text-green-500"  >
             <PlusCircleIcon className="h-5 w-5"/>
             <p>Create Playlist</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white"  >
+        <button className="flex items-center space-x-2 hover:text-green-500"  >
             <HeartIcon className="h-5 w-5 text-green-500"/>
             <p>Liked Songs</p>
         </button>
-        <button className="flex items-center space-x-2 hover:text-white" >
+        <button className="flex items-center space-x-2 hover:text-green-500" >
             <  RssIcon className="h-5 w-5"/>
             <p>Your Episodes</p>
         </button>
@@ -115,22 +115,13 @@ function Sidebar() {
        
        {/*Playlists*/}
         {playlists.map((playlist) =>(
-            <p key={playlist.id} className="cursor-pointer  hover:text-white"
+            <p key={playlist.id} className="cursor-pointer  hover:text-green-500"
             onClick={() => handlePlaylistClick(playlist.id)} // Use the click handler
             >
            {playlist.name} </p>
         ))}
 
-       {/*<p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>
-       <p className="cursor-pointer hover:text-white">Playlist name..</p>*/}
+       
        </div>
     </div>
   )
